bce32f37df2d263053ccac6abded8326aa75f90a
Victor FCh <v.faria@codebay-innovation.com>
2022-04-19 10:14:28 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,8 @@ import styled from 'styled-components'
 import { Formik, Form } from 'formik'
 import Input from './components/Input'
 import Button from './components/Button'
-
-const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	height: 100%;
-	align-items: center;
-`
-
-const Section = styled.section`
-	background-color: #eee;
-	border-top: solid 2px palevioletred;
-	padding: 20px 25px;
-	width: 500px;
-	box-shadow: 0 2px 3px rgb(0, 0, 0, 0.3 ) 
-`
+import Container from './components/Container'
+import Section from './components/Section'
 
 function App() {
 	const handleSubmit = () => {}
