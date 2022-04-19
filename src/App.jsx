321bc34b857e5947f99ea99dc99a3b4aec8d5abd
Victor FCh <v.faria@codebay-innovation.com>
2022-04-19 10:29:54 +0100

    add calculate balance

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import styled from 'styled-components'
 import { Formik, Form } from 'formik'
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
+import { useState } from 'react'
+
+const compoundInterest = (deposit, contribuition, years, rate) => {
+	let total = deposit
+	for (let i = 0; i < years; i++) {
+		total = (total + contribuition) * (rate + 1)
+	}
+
+	return Math.round(total)
+}
 
 function App() {
-	const handleSubmit = () => {}
+	const [balance, setBalance] = useState('')
+
+	const handleSubmit = ({deposit, contribuition, years, rate}) => {
+		const val = compoundInterest(Number(deposit), Number(contribuition), Number(years), Number(rate))
+		setBalance(val)
+	}
 
 	return (
 		<Container>
@@ -22,12 +36,13 @@ function App() {
 				>
 					<Form>
 						<Input name="deposit" label="Depósito inicial" />
-						<Input name="constribuition" label="Contribuición anual" />
+						<Input name="contribuition" label="Contribuición anual" />
 						<Input name="years" label="Años" />
 						<Input name="rate" label="Interés estimado" />
 						<Button>Calcular</Button>
 					</Form>
 				</Formik>
+				{balance && `Balance final: ${balance}`}
 			</Section>
 		</Container>
 	)
